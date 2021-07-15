cdee80f8aa6810e5dd6d9e38d3efa4c6beb897d4
Krisys <p.verbel@free.fr>
2021-07-15 16:53:16 +0200

    Negative feature

---
+++ SolastaModApi/Definitions/NegativeFeatureDefinition.cs
@@ -0,0 +1,58 @@
+﻿using SolastaModApi;
+using SolastaModApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolastaModApi.Definitions
+{
+    /// <summary>
+    /// Definition for a feature that removes another feature - can come handy for replacement features for instance
+    /// </summary>
+    public class NegativeFeatureDefinition : FeatureDefinition
+    {
+        public FeatureDefinition FeatureToRemove;
+    }
+
+    /// <summary>
+    /// Utility class for handling GUIDs for negative features
+    /// </summary>
+    public class NegativeFeatureUtility
+    {
+        private static Dictionary<string, string> featureToNegativeGuidDictionary;
+
+        public static Dictionary<string, string> FeatureToNegativeGuidDictionary
+        {
+            get
+            {
+                if (featureToNegativeGuidDictionary == null)
+                {
+                    featureToNegativeGuidDictionary = new Dictionary<string, string>();
+                }
+                return featureToNegativeGuidDictionary;
+            }
+        }
+    }
+
+    /// <summary>
+    /// To use NegativeFeatureBuilder on any feature, first add a new GUID for the negative feature in NegativeFeatureUtility.FeatureToNegativeGuidDictionary
+    /// </summary>
+    public class NegativeFeatureBuilder : BaseDefinitionBuilder<NegativeFeatureDefinition>
+    {
+        public NegativeFeatureBuilder(FeatureDefinition featureToRemove) : 
+            base(
+                $"{featureToRemove.Name}Negative",
+                NegativeFeatureUtility.FeatureToNegativeGuidDictionary.ContainsKey(featureToRemove.GUID) 
+                ? NegativeFeatureUtility.FeatureToNegativeGuidDictionary[featureToRemove.GUID] 
+                : "")
+        {
+            Definition.FeatureToRemove = featureToRemove;
+            Definition.GuiPresentation.SetHidden(true);
+        }
+
+        public static NegativeFeatureDefinition CreateAndAddToDB(FeatureDefinition featureToRemove)
+            => new NegativeFeatureBuilder(featureToRemove).AddToDB();
+    }
+}
---
+++ SolastaModApi/Extensions/CharacterBuildingManagerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using SolastaModApi.Definitions;
+
+namespace SolastaModApi.Extensions
+{
+    public class CharacterBuildingManagerExtensions
+    {
+        // This extension makes the CharacterBuildingManager.GrantFeaturesPatch take in account "negative features" that remove existing features
+        [HarmonyPatch(typeof(CharacterBuildingManager), "GrantFeatures")]
+        class GrantFeaturesPatch
+        {
+            static void Prefix(CharacterBuildingManager __instance, List<FeatureDefinition> grantedFeatures)
+            {
+                foreach (FeatureDefinition grantedFeature in grantedFeatures)
+                {
+                    NegativeFeatureDefinition negativeFeature = grantedFeature as NegativeFeatureDefinition;
+
+                    if (negativeFeature == null || negativeFeature.FeatureToRemove == null)
+                    {
+                        continue;
+                    }
+                    
+                    foreach (KeyValuePair<string, List<FeatureDefinition>> tagFeatures in __instance.HeroCharacter.ActiveFeatures)
+                    {
+                        tagFeatures.Value.RemoveAll(feature => feature.GUID.Equals(negativeFeature.FeatureToRemove.GUID) && feature.Name.Equals(negativeFeature.FeatureToRemove.Name));
+                    }
+                }
+            }
+        }
+    }
+}
